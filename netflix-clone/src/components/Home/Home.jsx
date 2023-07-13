defa4d07ba7387b0fc3e0d84b4b455a97848a602
AbhiiiDev <abhishek.2125cs1088@kiet.edu>
2023-07-14 01:45:56 +0530

    frontend-completed

--- netflix-clone/src/components/Home/Home.jsx
+++ netflix-clone/src/components/Home/Home.jsx
@@ -1,124 +1,113 @@
-import React,{useEffect,useState} from 'react'
-import './Home.scss'
-import axios from 'axios';
-import {Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import "./Home.scss";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
-const api='d31c000c5d916b22d4c9d1358bbf876c';
-const url="https://api.themoviedb.org/3"
-const upcoming='upcoming';
-const imgUrl="https://image.tmdb.org/t/p/original"
-const toprated='top_rated';
-const nowplaying='now_playing';
-const Popular='popular';
+const api = "d31c000c5d916b22d4c9d1358bbf876c";
+const url = "https://api.themoviedb.org/3";
+const upcoming = "upcoming";
+const imgUrl = "https://image.tmdb.org/t/p/original";
+const toprated = "top_rated";
+const nowplaying = "now_playing";
+const Popular = "popular";
 
+const Card = ({ img }) => <img className="card" src={img} alt="cover" />;
 
+const Row = ({ title, arr = [] }) => (
+  <div className="row">
+    <h2>{title}</h2>
 
-
-
-const Card=({img})=>(
-    <img className='card' src={img} alt='cover'/>
-)
-
-const Row=({title,arr=[]})=>(
-
-    <div className='row'>
-<h2>{title}</h2>
-
-
-<div>
-
-
-{
-arr.map((item,index)=>(
-
-    <Card key={index} img={`${imgUrl}${item.poster_path}`}/>
- 
-))
-}
-
-
-</div>
-
+    <div>
+      {arr.map((item, index) => (
+        <Card key={index} img={`${imgUrl}${item.poster_path}`} />
+      ))}
     </div>
-)
-
-
+  </div>
+);
 
 const Home = () => {
-    const [upcomingMovie, setupcomingMovie] = useState([])
-    const [topRated, settopRated] = useState([])
-    const [popular, setpopular] = useState([])
-    const [nowPlaying,setNowPlaying]=useState([])
-    const [genre, setGenre] = useState([])
-    useEffect(() => {
-      
-        const fetchUpcoming=async()=>{
+  const [upcomingMovie, setupcomingMovie] = useState([]);
+  const [topRated, settopRated] = useState([]);
+  const [popular, setpopular] = useState([]);
+  const [nowPlaying, setNowPlaying] = useState([]);
+  const [genre, setGenre] = useState([]);
+  useEffect(() => {
+    const fetchUpcoming = async () => {
+      {
+        const {
+          data: { results },
+        } = await axios.get(`${url}/movie/${upcoming}?api_key=${api}`); //upcoming movie
 
-{const {data :{results}}= await axios.get(`${url}/movie/${upcoming}?api_key=${api}`) ;  //upcoming movie
+        setupcomingMovie(results);
+      }
+    };
 
-       
-setupcomingMovie(results);
-}
-        }
+    const fetchRated = async () => {
+      const { data } = await axios.get(
+        `${url}/movie/${toprated}?api_key=${api}`
+      );
+      settopRated(data.results);
+    };
 
-        const fetchRated=async()=>{
-const {data}= await axios.get(`${url}/movie/${toprated}?api_key=${api}`) ; 
-settopRated(data.results);
-}
+    const fetchPopular = async () => {
+      const {
+        data: { results },
+      } = await axios.get(`${url}/movie/${Popular}?api_key=${api}`);
+      setpopular(results);
+    };
+    const fetchNowplaying = async () => {
+      const {
+        data: { results },
+      } = await axios.get(`${url}/movie/${nowplaying}?api_key=${api}`);
+      setNowPlaying(results);
+    };
+    const genre = async () => {
+      const {
+        data: { genres },
+      } = await axios.get(`${url}/genre/movie/list?api_key=${api}`);
+      setGenre(genres);
+      console.log(genres);
+    };
 
-const fetchPopular=async()=>{
-const {data:{results}}= await axios.get(`${url}/movie/${Popular}?api_key=${api}`) ; 
-setpopular(results);
-}
-const fetchNowplaying=async()=>{
-    const {data:{results}}= await axios.get(`${url}/movie/${nowplaying}?api_key=${api}`) ; 
-    setNowPlaying(results);
-    }
-    const genre=async()=>{
-        const { data : {genres},}= await axios.get(`${url}/genre/movie/list?api_key=${api}`) ; 
-        setGenre(genres);
-        console.log(genres);
-        }
-    
-
-fetchUpcoming();
-fetchNowplaying();
-fetchPopular();
-fetchRated();
-genre();
-
-
-
-
-    }, [])
-    
-
-
-
+    fetchUpcoming();
+    fetchNowplaying();
+    fetchPopular();
+    fetchRated();
+    genre();
+  }, []);
 
   return (
-    <section className="home">
-        <div className="banner" style={{
-            backgroundImage:popular[0]? `url(${`${imgUrl}/${popular[0].poster_path}`})`:"none"
-        }}>
-
-
+     <section className="home">
+            <div
+           className="banner"
+           style={{
+          backgroundImage: popular[3]
+            ? `url(${`${imgUrl}/${popular[3].poster_path}`})`
+            : "none",
+          }}
+            >
+           {popular[3] && <h1>{popular[3].original_title}</h1>}
+            {popular[3] && <p>{popular[3].overview}</p>}
     
+            <div>
+          <button>Play</button>
+
+          <button>My List</button>
         </div>
-        <Row title={"Upcoming Movies"} arr={upcomingMovie}/>
-        <Row title={"Popular"} arr={popular}/>
-        <Row title={"Now Playing"} arr={nowPlaying}/>
-        <Row title={"Top Rated"} arr={topRated}/>
-<div className="genreBox">
-{genre.map((item)=>(
-<Link key={item.id} to={`/genre/${item.id}`}>{item.name}</Link> 
-))}
-
-</div>
-
-        
+      </div>
+      <Row title={"Upcoming Movies"} arr={upcomingMovie} />
+      <Row title={"Popular"} arr={popular} />
+      <Row title={"Now Playing"} arr={nowPlaying} />
+      <Row title={"Top Rated"} arr={topRated} />
+      <div className="genreBox">
+        {genre.map((item) => (
+          <Link key={item.id} to={`/genre/${item.id}`}>
+            {item.name}
+          </Link>
+        ))}
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
